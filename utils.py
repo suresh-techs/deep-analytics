66fd2cc3d9139ef5b2c34e93ab7606ebd2c57675
John <suresh.yadam0713@gmail.com>
2022-09-19 13:37:38 +0530

    Updated analyse method

--- utils.py
+++ utils.py
@@ -1,7 +1,3 @@
 def analyse():
     """This method is used to analyse the data"""
-    if True:
-        print "Yes I will analyse the data"
-    else:
-        print "I will not analyse the data"
-    pass
+    print "Analysation completed"
